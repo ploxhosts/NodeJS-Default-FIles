7071651de91669d42bb50be1e7792da9dfdbdb1f
Gaeta <donald@plox.host>
2022-03-17 22:18:15 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -5,9 +5,9 @@ function execute(command, callback){
 
 console.log('Thank you for your purchase, we really appreciate it...\nYou may also git clone your repo via your startup tab Or follow the steps before to manually install them.\nTo get started follow these steps:\n0) Delete package.json & setup.js (These are just demo files)\n1) Upload your files\n2) Set the startup file in the Startup Parameters tab like node version etc\n3) make profit?\n\nIf you need any support, feel free to contact our team!')
 execute("node --version", function(data){
-  console.log(`Node Version: ${data}`);
+  console.log(`Node Version!: ${data}`);
 });
 execute("npm --version", function(data){
-  console.log(`NPM Version: ${data}`);
+  console.log(`NPM Version!: ${data}`);
 });
 setInterval(() => {}, 1 << 30);
